7ee753845ea004510eb05e2349e4944751e3ff78
naderhajlaoui <hajlaoui.nader@gmail.com>
2015-10-28 17:12:33 +0100

    Hello SpringBoot MicroServices

---
+++ Order-backend/src/main/java/order/backend/Order.java
@@ -0,0 +1,54 @@
+package main.java.order.backend;
+
+import java.util.Date;
+
+import main.java.order.backend.clone.Customer;
+import main.java.order.backend.clone.Product;
+
+public class Order {
+	private long id;
+	private long amount;
+	private Date dateOrder;
+	private Customer customer;
+	private Product product;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public long getAmount() {
+		return amount;
+	}
+
+	public void setAmount(long amount) {
+		this.amount = amount;
+	}
+
+	public Date getDateOrder() {
+		return dateOrder;
+	}
+
+	public void setDateOrder(Date dateOrder) {
+		this.dateOrder = dateOrder;
+	}
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+}
